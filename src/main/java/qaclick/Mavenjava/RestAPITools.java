85d8f6ca4ff04c42ef5b820425e74ba2837cf14b
Rutuja <rutujagorapwar1@gmail.com>
2020-08-04 11:37:50 +0530

    first-commit

---
+++ src/main/java/qaclick/Mavenjava/RestAPITools.java
@@ -0,0 +1,18 @@
+package qaclick.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class RestAPITools {
+
+	@Test
+	public void postJIRA()
+	{
+		System.out.println("postJIRA");
+	}
+	@Test
+	public void deleteTwitter() 
+	{
+		System.out.println("deleteTwitter");
+	}
+	
+}
